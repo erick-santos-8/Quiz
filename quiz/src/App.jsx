c0ab1a7ddca164a565949ec546de7b181cccc217
erick-santos-8 <esagosto@hotmail.com>
2024-05-13 14:51:21 -0300

    Context and Reducer initial

---
+++ quiz/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+import { useContext } from 'react'
+import { QuizContext } from './context/quiz'
+
+import Welcome from './components/Welcome'
+import Question from "./components/Question"
+
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div className='App'>
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome/>}
+      {quizState.gameStage === "Playing" && <Question/>}
+    </div>
+  )
+}
+
+export default App
